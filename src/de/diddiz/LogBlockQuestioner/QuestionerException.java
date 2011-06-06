c04a895a2402f63cf074cdbeedeae5e3b5268c21
Robin Kupper <robbzkupper@web.de>
2011-06-06 20:00:20 +0200

    Fixed errors when not using the Questioner plugin

---
+++ src/de/diddiz/LogBlockQuestioner/QuestionerException.java
@@ -0,0 +1,10 @@
+package de.diddiz.LogBlockQuestioner;
+
+public class QuestionerException extends Exception
+{
+	private static final long serialVersionUID = -1925764344890253869L;
+
+	public QuestionerException(String msg) {
+		super(msg);
+	}
+}
