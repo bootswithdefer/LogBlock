d4acbbd0bcb2def2ec82f85f741a37eca759c67e
Robin Kupper <robbzkupper@web.de>
2011-07-11 21:42:21 +0200

    Also not show no block type message for flintandsteel

--- src/de/diddiz/LogBlock/LBBlockListener.java
+++ src/de/diddiz/LogBlock/LBBlockListener.java
@@ -63,7 +63,7 @@ class LBBlockListener extends BlockListener
 	public void onBlockPlace(BlockPlaceEvent event) {
 		if (!event.isCancelled()) {
 			final int type = event.getBlock().getTypeId();
-			if (type == 0 && event.getItemInHand().getTypeId() == 43 && event.getBlock().getFace(BlockFace.DOWN).getTypeId() != 43) {
+			if (type == 0 && event.getItemInHand().getTypeId() != 43 && event.getBlock().getFace(BlockFace.DOWN).getTypeId() != 43 && event.getItemInHand() != null && event.getItemInHand().getTypeId() != 51) {
 				final Location loc = event.getBlock().getLocation();
 				System.out.println("[LogBlock] Bukkit provided no block type for the block placed by " + event.getPlayer().getName() + " at " + loc.getWorld().getName() + ":" + loc.getBlockX() + ":" + loc.getBlockY() + ":" + loc.getBlockZ() + ". Please check the conditions this happened under, and report it to Diddiz or Bukkit.");
 			}
