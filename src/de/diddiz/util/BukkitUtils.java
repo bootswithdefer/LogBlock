60db6779f406658b066c02d58ece76567a6aefdc
Robin Kupper <robbzkupper@web.de>
2011-08-13 13:38:07 +0200

    Removed "generic" as tree type for normal wood

--- src/de/diddiz/util/BukkitUtils.java
+++ src/de/diddiz/util/BukkitUtils.java
@@ -129,7 +129,7 @@ public class BukkitUtils
 	public static String materialName(int type, byte rawData) {
 		final Material mat = Material.getMaterial(type);
 		if (mat != null) {
-			if (type == 6 || type == 17 || type == 18 || type == 35 || type == 43 || type == 44) {
+			if ((type == 6 || type == 17 || type == 18) && rawData > 0 || type == 35 || type == 43 || type == 44) {
 				final MaterialData data = mat.getNewData(rawData);
 				if (data != null)
 					return data.toString().toLowerCase().replace('_', ' ').replaceAll("[^a-z ]", "");
