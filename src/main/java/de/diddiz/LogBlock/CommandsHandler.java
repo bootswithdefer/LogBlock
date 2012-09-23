4b16eba5ed5499c4dbb2e4ebe539bce2364c9717
Ammar Askar <ammar_askar@hotmail.com>
2012-09-23 22:02:35 +0500

    Parse chest access properly on teleport fixes #166 Replace old wiki link

--- src/main/java/de/diddiz/LogBlock/CommandsHandler.java
+++ src/main/java/de/diddiz/LogBlock/CommandsHandler.java
@@ -90,7 +90,7 @@ public class CommandsHandler implements CommandExecutor
 					sender.sendMessage(ChatColor.GOLD + "/lb prev|next -- Browse lookup result pages");
 					sender.sendMessage(ChatColor.GOLD + "/lb page -- Shows a specific lookup result page");
 					sender.sendMessage(ChatColor.GOLD + "/lb me -- Displays your stats");
-					sender.sendMessage(ChatColor.GOLD + "Look at github.com/DiddiZ/LogBlock/wiki/Commands for the full commands reference");
+					sender.sendMessage(ChatColor.GOLD + "Look at github.com/LogBlock/LogBlock/wiki/Commands for the full commands reference");
 				} else if (command.equals("params")) {
 					sender.sendMessage(ChatColor.DARK_AQUA + "LogBlock Query Parameters:");
 					sender.sendMessage(ChatColor.GOLD + "Use doublequotes to escape a keyword: world \"world\"");
@@ -537,6 +537,9 @@ public class CommandsHandler implements CommandExecutor
 		public void run() {
 			try {
 				params.needCoords = true;
+				if (params.bct == BlockChangeType.CHESTACCESS || params.types.isEmpty() || Block.inList(params.types, 23) || Block.inList(params.types, 54) || Block.inList(params.types, 61) || Block.inList(params.types, 62)) {
+					params.needChestAccess = true;
+				}
 				params.limit = 1;
 				params.sum = SummarizationMode.NONE;
 				conn = logblock.getConnection();
