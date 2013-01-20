3721746f25c90d9c21c4affcba6f2a5e2677de53
Ammar Askar <ammar@ammaraskar.com>
2013-01-21 02:37:31 +0500

    Add legacy escaping for dumped logs

--- src/main/java/de/diddiz/LogBlock/Consumer.java
+++ src/main/java/de/diddiz/LogBlock/Consumer.java
@@ -439,8 +439,9 @@ public class Consumer extends TimerTask
 			final String table = getWorldConfig(loc.getWorld()).table;
 			final String[] inserts = new String[ca != null || signtext != null ? 2 : 1];
 			inserts[0] = "INSERT INTO `" + table + "` (date, playerid, replaced, type, data, x, y, z) VALUES (FROM_UNIXTIME(" + date + "), " + playerID(playerName) + ", " + replaced + ", " + type + ", " + data + ", '" + loc.getBlockX() + "', " + loc.getBlockY() + ", '" + loc.getBlockZ() + "');";
-			if (signtext != null)
-				inserts[1] = "INSERT INTO `" + table + "-sign` (id, signtext) values (LAST_INSERT_ID(), '" + signtext + "');";
+			if (signtext != null) {
+				inserts[1] = "INSERT INTO `" + table + "-sign` (id, signtext) values (LAST_INSERT_ID(), '" + signtext.replace("\\", "\\\\").replace("'", "\\'") + "');";
+			}
 			else if (ca != null)
 				inserts[1] = "INSERT INTO `" + table + "-chest` (id, itemtype, itemamount, itemdata) values (LAST_INSERT_ID(), " + ca.itemType + ", " + ca.itemAmount + ", " + ca.itemData + ");";
 			return inserts;
@@ -555,7 +556,7 @@ public class Consumer extends TimerTask
 
 		@Override
 		public String[] getInserts() {
-			return new String[]{"INSERT INTO `lb-chat` (date, playerid, message) VALUES (FROM_UNIXTIME(" + date + "), " + playerID(playerName) + ", '" + message + "');"};
+			return new String[]{"INSERT INTO `lb-chat` (date, playerid, message) VALUES (FROM_UNIXTIME(" + date + "), " + playerID(playerName) + ", '" + message.replace("\\", "\\\\").replace("'", "\\'") + "');"};
 		}
 
 		@Override
