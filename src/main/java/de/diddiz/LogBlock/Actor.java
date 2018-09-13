2bb9e099592b7cb456832bc8b1a83893c333e247
Brokkonaut <hannos17@gmx.de>
2018-09-13 02:04:33 +0200

    Cleanup Actor.equals() and .hashCode() + some formatting

--- src/main/java/de/diddiz/LogBlock/Actor.java
+++ src/main/java/de/diddiz/LogBlock/Actor.java
@@ -19,21 +19,16 @@ public class Actor {
 
     @Override
     public int hashCode() {
-        int hash = 5;
-        hash = 79 * hash + (this.UUID != null ? this.UUID.hashCode() : 0);
-        return hash;
+        return this.UUID != null ? this.UUID.hashCode() : 0;
     }
 
     @Override
     public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
+        if (obj == null || getClass() != obj.getClass()) {
             return false;
         }
         final Actor other = (Actor) obj;
-        return ((this.UUID == null && other.UUID == null) || this.UUID.equals(other.UUID));
+        return this.UUID == null ? other.UUID == null : this.UUID.equals(other.UUID);
     }
 
     final String name;
@@ -111,20 +106,21 @@ public class Actor {
         }
 
     }
-/**
- * Generate an Actor object from a String name, trying to guess if it's an online player
- * and if so, setting the UUID accordingly.  This only checks against currently online
- * players and is a "best effort" attempt for use with the pre-UUID API
- * <p>
- * If you know something is an entity (player or otherwise) use the {@link #actorFromEntity(org.bukkit.entity.Entity) }
- * or {@link #actorFromEntity(org.bukkit.entity.EntityType) } methods
- * <p>
- * If you know something is a server effect (like gravity) use {@link #Actor(java.lang.String)}
- * @deprecated Only use this if you have a String of unknown origin
- * 
- * @param actorName String of unknown origin
- * @return 
- */
+
+    /**
+     * Generate an Actor object from a String name, trying to guess if it's an online player
+     * and if so, setting the UUID accordingly.  This only checks against currently online
+     * players and is a "best effort" attempt for use with the pre-UUID API
+     * <p>
+     * If you know something is an entity (player or otherwise) use the {@link #actorFromEntity(org.bukkit.entity.Entity) }
+     * or {@link #actorFromEntity(org.bukkit.entity.EntityType) } methods
+     * <p>
+     * If you know something is a server effect (like gravity) use {@link #Actor(java.lang.String)}
+     * @deprecated Only use this if you have a String of unknown origin
+     * 
+     * @param actorName String of unknown origin
+     * @return 
+     */
     public static Actor actorFromString(String actorName) {
         Collection<? extends Player> players = Bukkit.getServer().getOnlinePlayers();
         for (Player p : players) {
@@ -132,8 +128,8 @@ public class Actor {
                 return actorFromEntity(p);
             }
         }
-    // No player found online with that name, assuming non-player entity/effect
-    return new Actor(actorName);
+        // No player found online with that name, assuming non-player entity/effect
+        return new Actor(actorName);
     }
 
     public static boolean isValidUUID(String uuid) {
